6209971fbfb65df5cdd6827ecdf92372261939fb
Pallies <palliescie@gmail.com>
2021-09-27 10:35:21 +0200

    TP 06 bootstrap

--- TP-jquery/script/script-jquery.js
+++ TP-jquery/script/script-jquery.js
@@ -1,5 +1,16 @@
+
 $(function () {
+    var $navMiddleBar = $(".navbar_middle");
   $(".navbar_top-close").click(function () {
     $(this).parent().remove();
   });
+$navMiddleBar.find(".navbar_middle-link").click(function(){
+    console.log(this)
+    $(".navbar_top:after")
+      .css({ "z-index": "20", opacity: "1" })
+      .add(function () {
+        console.log(this);
+   
+      });
+});
 });
