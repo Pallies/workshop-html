afd0fb1ca2a4f85db52444367f4dab7c07ac83b5
Pallies <palliescie@gmail.com>
2021-09-29 00:01:37 +0200

    add data form

---
+++ TP atelier Bootstrap/script/add-user.js
@@ -0,0 +1,20 @@
+var createNewCard = function () {};
+
+$(() => {
+  var newNameUser = localStorage.getItem("name");
+  var newFirstnameUser = localStorage.getItem("firstname");
+  var newBirthdayUser = localStorage.getItem("birthday");
+  var newAddressUser = localStorage.getItem("address");
+  var newSocialSecuUser = localStorage.getItem("social_security");
+  localStorage.clear();
+
+//   console.log(
+//     "%c⧭",
+//     "color: #00258c",
+//     newNameUser,
+//     newFirstnameUser,
+//     newBirthdayUser,
+//     newAddressUser,
+//     newSocialSecuUser
+//   );
+});
---
+++ TP atelier Bootstrap/script/creer.js
@@ -0,0 +1,39 @@
+
+
+// Events
+var btn_comfirm = document.getElementById("modalSubmit");
+var btn_create = document.getElementById("form-valid");
+
+// Champs
+var forms = document.querySelectorAll(".form-list");
+var modals = document.querySelectorAll(".modal-list");
+var User = {};
+var errors =[];
+
+// save in locaStore
+var saveToLocalStorage = function () {
+  $("#confirm_modal").modal("hide");
+  for (let [key, value] of Object.entries(User)) {
+    localStorage.setItem(key, value);
+  }
+};
+
+// form in Modal
+var transfertInfoUserToModal = function () {
+  forms.forEach((form, i) => {
+    User[form.name] = form.value;
+    modals[i].innerHTML = form.value;
+    errors.push(formIsValide(form));
+  });
+  errors.forEach(e=>{
+    // TODO gestion des erreurs
+    console.log('%c⧭', 'color: #f279ca', e);
+  })
+  $("#confirm_modal").modal("show");
+};
+
+// Elements to Events
+btn_create.addEventListener("click", transfertInfoUserToModal);
+btn_comfirm.addEventListener("click", saveToLocalStorage);
+
+
